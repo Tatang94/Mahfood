b25b45c36b357fb64dad8eef2b5e1d9b90734e4b
Tatang94 <35641333+Tatang94@users.noreply.github.com>
2025-06-23 17:33:37 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import serverless from 'serverless-http';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -10,6 +11,5 @@ app.get('/api/test', (req, res) => {
   res.status(200).json({ message: 'API is working' });
 });
 
-// Konversi express ke handler serverless
 const handler = serverless(app);
 export default handler;
